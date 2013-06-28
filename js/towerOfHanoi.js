634166ad46c70cc474011fa24aba27b575269e77
randy <randay@gmail.com>
2013-06-28 19:24:32 -0400

    recursive alg in plain english. needs to be implemented

--- js/towerOfHanoi.js
+++ js/towerOfHanoi.js
@@ -7,39 +7,20 @@ function towerOfHanoi(size) {
     }
     //printBoard(myBoard);
     while (!isSolved(myBoard) && loops < 15) {
-       move(myBoard, size);
+       myBoard = move(myBoard, size);
        loops++;
     }
     console.log('finished');
 }
 
 function move(board, size) {
-    //printBoard(board);
-    
-    //var topOfZeroLT
-    if (board[0][board[0].length-1] < board[1][board[1].length-1]) {
-        // compare top of 0 with top of 1, move if can
-        console.log('moving top of 0 to top of 1');
-        board[1].push(board[0].pop());
-    } else if (board[0][board[0].length-1] < board[2][board[2].length-1]) {
-        // compare top of 0 with top of 2, move if can
-        console.log("moving top of 0 to top of 2");
-        board[2].push(board[0].pop());
-    } else if (board[1][board[1].length-1] < board[2][board[2].length-1]) {
-        // compare top of 1 with top of 2, move if can
-        console.log("moving top of 1 to top of 2");
-        board[2].push(board[1].pop());
-    } else if (board[2][board[2].length-1] < board[0][board[0].length -1 ]) {
-        // compare top of 2 with top of 0, move if can
-        board[0].push(board[2].pop());
-        console.log("moving top of 2 to top of 0");
-    }
-    // move top of 2 to top of 1 
-    
-    // move top of 0 to 1
-    
-    // move top of 0 to 2
-    
+/*
+
+1. move n−1 discs from A to B. This leaves disc n alone on peg A
+2. move disc n from A to C
+3. move n−1 discs from B to C so they sit on disc n
+
+*/
     printBoard(board);
 }
 
