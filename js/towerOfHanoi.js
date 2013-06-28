5e91292443e015fd897b5bca1b59dbf973202c1a
randy <randay@gmail.com>
2013-06-28 19:23:05 -0400

    wip read wiki and see recursive alg in 3 steps will try that next

--- js/towerOfHanoi.js
+++ js/towerOfHanoi.js
@@ -1,36 +1,26 @@
+var loops = 0;
+
 function towerOfHanoi(size) {
     var myBoard = [[],[],[]];
     for (var ii=size;ii>0;ii--) {
         myBoard[0][ii % size] = size - (ii % size) -1;
     }
     //printBoard(myBoard);
-    move(myBoard, size);
+    while (!isSolved(myBoard) && loops < 15) {
+       move(myBoard, size);
+       loops++;
+    }
+    console.log('finished');
 }
 
 function move(board, size) {
-    // push to move top of 0 to peg 1
-    
-    // push to move top of 0 to peg 2
-    
-    // try to clear peg 2 for top of 0
-    
-    
-    
-    
-    
-    
-    // keep taking from peg 0 until it is empty
-
-    
-    
-    
-    printBoard(board);
+    //printBoard(board);
     
     //var topOfZeroLT
     if (board[0][board[0].length-1] < board[1][board[1].length-1]) {
         // compare top of 0 with top of 1, move if can
         console.log('moving top of 0 to top of 1');
-        board[i].push(board[0].pop());
+        board[1].push(board[0].pop());
     } else if (board[0][board[0].length-1] < board[2][board[2].length-1]) {
         // compare top of 0 with top of 2, move if can
         console.log("moving top of 0 to top of 2");
@@ -50,17 +40,32 @@ function move(board, size) {
     
     // move top of 0 to 2
     
-    //printBoard(board);
-    console.log('size: ', size);
-    // if (board[0].length === (size) && board[1].length === 0) {
-    //     //if (board[0][board[0].length-1] < )
-    //     console.log('in here');
-    // }
-     
+    printBoard(board);
 }
 
 function printBoard(board) {
     console.log('[' + board[0] + '],[' + board[1] + '],[' + board[2] + ']]');   
+}
+
+function isSolved(board) {
+    var prev = -1;
+    
+    if (board[0].length === 0) {
+        if (board[2].length === 0) {
+            for (var ii = 0; ii < board[1].length; ii++) {
+                if (board[1][ii] !== ii )
+                    return false;
+            }
+            return true;
+        } else if (board[1].length === 0) {
+            for (var ii = 0; ii < board[2].length; ii++) {
+                if (board[2][ii] !== ii )
+                    return false;
+            }
+            return true;            
+        }
+    }
+    return false;
 }
 
 function canMoveTo(board, dest) {
