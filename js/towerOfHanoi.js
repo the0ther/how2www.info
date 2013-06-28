9d3a838a943806ef48db82e1aee994d6f6c3cb32
randy <randay@gmail.com>
2013-06-28 17:59:56 -0400

    wip

--- js/towerOfHanoi.js
+++ js/towerOfHanoi.js
@@ -4,10 +4,18 @@ function towerOfHanoi(size) {
         myBoard[0][ii % size] = size - (ii % size) -1;
     }
     console.log(myBoard[0]);
+    move(myBoard);
 }
 
 function move(board) {
-    
+    // keep taking from peg 0 until it is empty
+        // use .pop() on the arrays. the pegs are stacks
+    console.log(board);
+    if (board[0].length > 0 && board[1].length == 0) {
+        //if (board[0][board[0].length-1] < )
+        console.log('in here');
+    }
+    // 
 }
 
 function canMoveTo(board, dest) {
