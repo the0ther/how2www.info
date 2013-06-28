5f20ccfe4dee8cfbec91d70a103f8e11556946db
randy <randay@gmail.com>
2013-06-28 17:30:50 -0400

    adding knights tour

---
+++ js/knights-tour.js
@@ -0,0 +1,15 @@
+function knightsTour() {
+    console.log('entering knightsTour()');
+    var board = [];
+    var startPos = 0;
+}
+
+function move(board) {
+    console.log('entering move()');
+}
+
+function calcPosition() {
+    console.log('entering calcPosition()');
+}
+
+knightsTour();
