9ec5e3f63a49230a7435ffb0352b2f83c747d404
randy <randay@gmail.com>
2013-06-28 17:50:33 -0400

    wip

--- js/towerOfHanoi.js
+++ js/towerOfHanoi.js
@@ -1,12 +1,16 @@
 function towerOfHanoi(size) {
     var myBoard = [[],[],[]];
-    for (var ii=size;ii>=0;ii--) {
-        myBoard[0][ii] = ii % size;
+    for (var ii=size;ii>0;ii--) {
+        myBoard[0][ii % size] = size - (ii % size) -1;
     }
     console.log(myBoard[0]);
 }
 
 function move(board) {
+    
+}
+
+function canMoveTo(board, dest) {
     
 }
 
